c66eca6b1c4cf70be94953a95d5ac3122dc85f91
chenyk1990 <chenyk1990@gmail.com>
2016-05-04 18:16:31 -0400

    up

---
+++ projects/specfem3d_globe/compile.titan.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#see: ~/.bashrc
+source /etc/profile
+
+module list
+
+## cross-compiling:
+
+## compilation for xcreate_header_file
+## to run on login node
+echo
+echo "compiling xcreate_header_file..."
+echo
+
+# changes target rules
+if [ -e src/create_header_file/rules.mk ]; then
+  # new makefile - rules
+  sed -i 's:@-rm -f $@.*:@-rm -f $@; cp bin.header_file/xcreate_header_file $E/:' src/create_header_file/rules.mk
+else
+  # old makefile
+  sed -i "s:(cd ../create_header_file; make):#(cd ../create_header_file; make):g" src/specfem3D/Makefile
+  sed -i "s:(cd ../create_header_file; make):#(cd ../create_header_file; make):g" src/auxiliaries/Makefile
+fi
+
+##
+## compiles xcreate_header_file
+## (for static compilation)
+module switch craype-interlagos craype-target-native
+echo
+module list
+echo
+
+make clean
+make xcreate_header_file
+#if [ ! -e bin/xcreate_header_file ]; then exit; fi
+rm -rf bin.header_file
+cp -rp bin bin.header_file
+
+module switch craype-target-native craype-interlagos
+echo
+module list
+echo
+
+
+# forward simulations
+#
+# only needed to produce synthetics for flexwin/measurements
+#
+echo
+echo "compiling forward simulations..."
+echo
+make clean
+
+rm -rf OUTPUT_FILES/*
+rm -rf bin.forward
+
+mkdir -p bin
+cp bin.header_file/xcreate_header_file bin/
+make -j 8 xmeshfem3D
+make -j 8 xspecfem3D
+
+cp -rp bin bin.forward
+cp OUTPUT_FILES/* bin.forward/
---
+++ projects/specfem3d_globe/configure.titan.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#see: ~/.bashrc
+source /etc/profile
+module list
+
+mpif90=ftn
+mpicc=cc
+f90=ftn
+cc=cc
+
+## gnu compilers
+warn="-Wunused -Waliasing -Wampersand -Wcharacter-truncation -Wline-truncation -Wsurprising -Wno-tabs -Wunderflow"
+flags="-O3 -mcmodel=medium $warn"
+cflags=""
+
+##################################################
+# with asdf, adios and cuda5 support
+# 1. Make sure you download and compiled the asdf library(https://github.com/SeismicData/asdf-library).
+# 2. load adios and cuda library from titan system module
+
+./configure --with-asdf ASDF_LIBS="/ccs/home/lei/bin/asdf-library/build/lib/libasdf.a" --with-adios --with-cuda=cuda5 --host=x86_64-unknown-linux-gnu MPIF90=$mpif90 F90=$f90 CC=$cc FLAGS_CHECK="$flags" FCFLAGS="" CFLAGS="$cflags" CUDA_INC="$CUDATOOLKIT_HOME/include" CUDA_LIB="$CUDATOOLKIT_HOME/lib64" MPI_INC="$CRAY_MPICH2_DIR/include"
+
+##################################################
+# with adios and cuda
+# load adios and cuda library from titan system module
+
+#./configure --with-adios --with-cuda=cuda5 --host=x86_64-unknown-linux-gnu MPIF90=$mpif90 F90=$f90 CC=$cc FLAGS_CHECK="$flags" FCFLAGS="" CFLAGS="$cflags" CUDA_INC="$CUDATOOLKIT_HOME/include" CUDA_LIB="$CUDATOOLKIT_HOME/lib64" MPI_INC="$CRAY_MPICH2_DIR/include"
+
+##
+## setup
+##
+echo
+echo "modifying mesh_constants_cuda.h..."
+sed -i "/ENABLE_VERY_SLOW_ERROR_CHECKING/ c\#undef ENABLE_VERY_SLOW_ERROR_CHECKING" src/gpu/mesh_constants_cuda.h
+
+echo
+echo "done"
+echo
+
