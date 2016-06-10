25ea71f154b558096cf391a1fcf480f432e3102e
chenyk1990 <chenyk1990@gmail.com>
2016-06-10 14:49:45 -0400

    Fri Jun 10 14:49:35 EDT 2016

--- projects/gmt/examples/ex36/example_36.sh
+++ projects/gmt/examples/ex36/example_36.sh
@@ -6,16 +6,31 @@
 # GMT progs:    psxy, makecpt, grdimage, sphinterpolate
 # Unix progs:   rm
 #
-ps=example_36.ps
+ps=example_36_t2.ps
 # Interpolate data of Mars radius from Mariner9 and Viking Orbiter spacecrafts
-gmt makecpt -Crainbow -T-7000/15000/1000 -Z > tt.cpt
+#gmt makecpt -Crainbow -T-7000/15000/1000 -Z > tt.cpt
 # Piecewise linear interpolation; no tension
-gmt sphinterpolate mars370.txt -Rg -I1 -Q0 -Gtt.nc
-gmt grdimage tt.nc -JH0/6i -Bag -Ctt.cpt -P -Xc -Y7.25i -K > $ps
-gmt psxy -Rg -J -O -K mars370.txt -Sc0.05i -G0 -B30g30 -Y-3.25i >> $ps
+#gmt sphinterpolate mars370.txt -Rg -I1 -Q0 -Gtt.nc
+#gmt grdimage tt.nc -JH0/6i -Bag -Ctt.cpt -P -Xc -Y7.25i -K > $ps
+#gmt psxy -Rg -J -O -K mars370.txt -Sc0.05i -G0 -B30g30 -Y-3.25i >> $ps
 # Smoothing
-gmt sphinterpolate mars370.txt -Rg -I1 -Q3 -Gtt.nc
-gmt grdimage tt.nc -J -Bag -Ctt.cpt  -Y-3.25i -O -K >> $ps
-gmt psxy -Rg -J -O -T >> $ps
+#gmt sphinterpolate mars370.txt -Rg -I1 -Q3 -Gtt.nc
+#gmt grdimage tt.nc -J -Bag -Ctt.cpt  -Y-3.25i -O -K >> $ps
+#gmt psxy -Rg -J -O -T >> $ps 
 # cleanup
 #rm -f tt.cpt tt.nc
+
+
+makecpt -Crainbow -T-7000/15000/1000 -Z > tt.cpt
+# Piecewise linear interpolation; no tension
+sphinterpolate mars370.txt -Rg -I1 -Q0 -Gtt.nc
+grdimage tt.nc -JH0/6i -Bag -Ctt.cpt -P -Xc -Y7.25i -K > $ps
+psxy -Rg -J -O -K mars370.txt -Sc0.05i -G0 -B30g30 -Y-3.25i >> $ps
+# Smoothing
+sphinterpolate mars370.txt -Rg -I1 -Q3 -Gtt.nc
+grdimage tt.nc -JH0/6i -Bag -Ctt.cpt  -Y-3.25i -O -K -V >> $ps
+
+
+open $ps
+
+
--- projects/gmt/examples/ex39/example_39.sh
+++ projects/gmt/examples/ex39/example_39.sh
@@ -6,7 +6,7 @@
 # GMT progs:    psscale, pstext, makecpt, grdimage, grdgradient, sph2grd
 # Unix progs:   rm
 #
-ps=example_39.ps
+ps=example_39_t.ps
 
 # Evaluate the first 180, 90, and 30 order/degrees of Venus spherical
 # harmonics topography model, skipping the L = 0 term (radial mean).
@@ -14,20 +14,23 @@ ps=example_39.ps
 # Wieczorek, M. A., Gravity and topography of the terrestrial planets,
 #   Treatise on Geophysics, 10, 165-205, doi:10.1016/B978-044452748-6/00156-5, 2007
 
-gmt sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv1.nc -F1/1/25/30
-gmt sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv2.nc -F1/1/85/90
-gmt sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv3.nc -F1/1/170/180
-gmt grd2cpt v3.nc -Crainbow -E16 -Z > t.cpt
-gmt grdgradient v1.nc -Nt0.75 -A45 -Gvint.nc
-gmt grdimage v1.nc -Ivint.nc -JG90/30/5i -P -K -Bg -Ct.cpt -X3i -Y1.1i > $ps
+sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv1.nc -F1/1/25/30
+sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv2.nc -F1/1/85/90
+sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv3.nc -F1/1/170/180
+grd2cpt v3.nc -Crainbow -E16 -Z > t.cpt
+grdgradient v1.nc -Nt0.75 -A45 -Gvint.nc
+grdimage v1.nc -Ivint.nc -JG90/30/5i -P -K -Bg -Ct.cpt -X3i -Y1.1i > $ps
 echo 4 4.5 L = 30 | gmt pstext -R0/6/0/6 -Jx1i -O -K -Dj0.2i -F+f16p+jLM -N >> $ps
-gmt psscale --FORMAT_FLOAT_MAP="%'g" -Ct.cpt -O -K -Dx1.25i/-0.2i+jTC+w5.5i/0.1i+h -Bxaf -By+lm >> $ps
-gmt grdgradient v2.nc -Nt0.75 -A45 -Gvint.nc
-gmt grdimage v2.nc -Ivint.nc -JG -O -K -Bg -Ct.cpt -X-1.25i -Y1.9i >> $ps
+psscale --FORMAT_FLOAT_MAP="%'g" -Ct.cpt -O -K -Dx1.25i/-0.2i+jTC+w5.5i/0.1i+h -Bxaf -By+lm >> $ps
+grdgradient v2.nc -Nt0.75 -A45 -Gvint.nc
+grdimage v2.nc -Ivint.nc -JG -O -K -Bg -Ct.cpt -X-1.25i -Y1.9i >> $ps
 echo 4 4.5 L = 90 | gmt pstext -R0/6/0/6 -Jx1i -O -K -Dj0.2i -F+f16p+jLM -N >> $ps
-gmt sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv3.nc -F1/1/170/180
-gmt grdgradient v3.nc -Nt0.75 -A45 -Gvint.nc
-gmt grdimage v3.nc -Ivint.nc -JG -O -K -Bg -Ct.cpt -X-1.25i -Y1.9i >> $ps
+sph2grd VenusTopo180.txt -I1 -Rg -Ng -Gv3.nc -F1/1/170/180
+grdgradient v3.nc -Nt0.75 -A45 -Gvint.nc
+grdimage v3.nc -Ivint.nc -JG -O -K -Bg -Ct.cpt -X-1.25i -Y1.9i >> $ps
 echo 4 4.5 L = 180 | gmt pstext -R0/6/0/6 -Jx1i -O -K -Dj0.2i -F+f16p+jLM -N >> $ps
 echo 3.75 5.4 Venus Spherical Harmonic Model | gmt pstext -R0/6/0/6 -Jx1i -O -F+f24p+jCM -N >> $ps
 rm -f v*.nc t.cpt
+
+
+open $ps
