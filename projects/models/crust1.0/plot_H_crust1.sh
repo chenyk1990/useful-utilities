3cb61088902f90c0298fd9c0718e11c1e5f56302
chenyk1990 <chenyk1990@gmail.com>
2016-06-10 00:46:36 -0400

    up

--- projects/models/crust1.0/plot_H_crust1.sh
+++ projects/models/crust1.0/plot_H_crust1.sh
@@ -8,7 +8,7 @@ gmtset BASEMAP_TYPE FANCY PLOT_DEGREE_FORMAT ddd:mm:ssF GRID_CROSS_SIZE_PRIMARY 
 
 range="-121/-109/48/58"
 # awk '{print $1, $2, $3}' crust1.0.model | surface -Gtemp.grd -I0.05/0.05 -R$range -T0 -Lld -Lud
-awk '{print $2, $1, $3}' crust1.xyz | surface -Gtemp.grd -I0.05/0.05 -R$range -T0 -Lld -Lud
+awk '{print $1, $2, $4}' crust1.xyz | surface -Gtemp.grd -I0.05/0.05 -R$range -T0 -Lld -Lud
 # grdmath -V mask.grd temp.grd MUL = H-map.grd
 #grd2cpt temp.grd -Crainbow -I  -E50 -D > my_crust1.cpt
 makecpt -T34/54/0.2 -Crainbow -I -D  > my_crust1.cpt
@@ -19,9 +19,10 @@ pscoast -JL -R -Dl -N2/3 -N1 -O -W1/100 -K -O -V >> $PSNAME
 
 psxy AB_Geo_new.txt -JL -R -W4/20/20/20 -M"X" -K -O -V >> $PSNAME
 
- grdcontour temp.grd -Ch_contour.cpt -J -R -W3/180/130/130 -A2.0f9 -K -O -V >> $PSNAME
+grdcontour temp.grd -Ch_contour.cpt -J -R -W3/180/130/130 -A2.0f9 -K -O -V >> $PSNAME
 
 psscale -D6.2i/1.i/1.5i/0.33i -Cmy_crust1.cpt -B5 -O -V >> $PSNAME
+evince $PSNAME &
 
 # ps2raster $PSNAME -A -Tef -V 
 #gv $PSNAME
