1fbeafec62a7ae677523ce8fb0d4e859e9d835e9
chenyk1990 <chenyk1990@gmail.com>
2017-12-27 23:52:52 -0500

    Wed Dec 27 23:52:50 EST 2017

--- projects/CWPSRC/su/lib/getSPSfield.c
+++ projects/CWPSRC/su/lib/getSPSfield.c
@@ -42,81 +42,81 @@ struct linepars {
 struct linepars *pparse;
 struct linepars *xparse;
 
-//  Version 0 record description:
+/*  Version 0 record description:*/
 struct linepars pparse0[] = {
-      {  0, 1, "%c","RecTyp"   },  //  0 Label (R or S)
-      {  1,16, "%f","LName"    },  //  1 Line name       (string in std)
-      { 17, 8, "%f","PNum"     },  //  2 Point number    (string in std)
-      { 25, 1, "%d","PInd"     },  //  3 Point index
-      { 26, 2, "%d","PCode"    },  //  4 Point code      (string in std)
-      { 28, 4, "%d","StaCor"   },  //  5 Static Correction
-      { 32, 4, "%f","PDepth"   },  //  6 Point Depth
-      { 36, 4, "%d","Datum"    },  //  7 Seismic datum
-      { 40, 2, "%d","UpHole"   },  //  8 Uphole Time
-      { 42, 4, "%f","WDept"    },  //  9 Water Depth
-      { 46, 9, "%lf","MapX"    },  // 10 Map Grid Easting
-      { 55,10, "%lf","MapY"    },  // 11 Map Grid Northing
-      { 65, 6, "%f","Elev"     },  // 12 Surface Elevation
-      { 71, 3, "%d","YDay"     },  // 13 Day of Year
-      { 74, 6, "%d","Time"     },  // 14 Time of day     (2I2 in std)
+      {  0, 1, "%c","RecTyp"   },  /*  0 Label (R or S)*/
+      {  1,16, "%f","LName"    },  /*  1 Line name       (string in std)*/
+      { 17, 8, "%f","PNum"     },  /*  2 Point number    (string in std)*/
+      { 25, 1, "%d","PInd"     },  /*  3 Point index*/
+      { 26, 2, "%d","PCode"    },  /*  4 Point code      (string in std)*/
+      { 28, 4, "%d","StaCor"   },  /*  5 Static Correction*/
+      { 32, 4, "%f","PDepth"   },  /*  6 Point Depth*/
+      { 36, 4, "%d","Datum"    },  /*  7 Seismic datum*/
+      { 40, 2, "%d","UpHole"   },  /*  8 Uphole Time*/
+      { 42, 4, "%f","WDept"    },  /*  9 Water Depth*/
+      { 46, 9, "%lf","MapX"    },  /* 10 Map Grid Easting*/
+      { 55,10, "%lf","MapY"    },  /* 11 Map Grid Northing*/
+      { 65, 6, "%f","Elev"     },  /* 12 Surface Elevation*/
+      { 71, 3, "%d","YDay"     },  /* 13 Day of Year*/
+      { 74, 6, "%d","Time"     },  /* 14 Time of day     (2I2 in std)*/
 };
 
 
 struct linepars xparse0[] = {
-      {  0, 1, "%c","RecTyp"   },  //  0 Label X
-      {  1, 6, "%s","TapeNo"   },  //  1 Tape Number     (String in std)
-      {  7, 4, "%d","RecNum"   },  //  2 Field Record Number
-      { 11, 1, "%d","RecInc"   },  //  3 Field Record Increment
-      { 12, 1, "%c","InstCode" },  //  4 Instrument Code (string in std)
-      { 13,16, "%f","SLName"   },  //  5 Source Line Name(string in std)
-      { 29, 8, "%f","PNum"     },  //  6 Point number    (string in std)
-      { 37, 1, "%d","PIndex"   },  //  7 Point Index
-      { 38, 4, "%d","FromChan" },  //  8 From Channel
-      { 42, 4, "%d","ToChan"   },  //  9 To Channel
-      { 46, 1, "%d","ChanInc"  },  // 10 Channel Increment
-      { 47,16, "%f","RLName"   },  // 11 Receiver Line name(string in std)
-      { 63, 8, "%f","FromRecv" },  // 12 From Receiver   (string in std)
-      { 71, 8, "%f","ToRecv"   },  // 13 To Receiver     (string in std)
-      { 79, 1, "%d","RenIx"    }   // 14 Receiver Index
+      {  0, 1, "%c","RecTyp"   },  /*  0 Label X*/
+      {  1, 6, "%s","TapeNo"   },  /*  1 Tape Number     (String in std)*/
+      {  7, 4, "%d","RecNum"   },  /*  2 Field Record Number*/
+      { 11, 1, "%d","RecInc"   },  /*  3 Field Record Increment*/
+      { 12, 1, "%c","InstCode" },  /*  4 Instrument Code (string in std)*/
+      { 13,16, "%f","SLName"   },  /*  5 Source Line Name(string in std)*/
+      { 29, 8, "%f","PNum"     },  /*  6 Point number    (string in std)*/
+      { 37, 1, "%d","PIndex"   },  /*  7 Point Index*/
+      { 38, 4, "%d","FromChan" },  /*  8 From Channel*/
+      { 42, 4, "%d","ToChan"   },  /*  9 To Channel*/
+      { 46, 1, "%d","ChanInc"  },  /* 10 Channel Increment*/
+      { 47,16, "%f","RLName"   },  /* 11 Receiver Line name(string in std)*/
+      { 63, 8, "%f","FromRecv" },  /* 12 From Receiver   (string in std)*/
+      { 71, 8, "%f","ToRecv"   },  /* 13 To Receiver     (string in std)*/
+      { 79, 1, "%d","RenIx"    }   /* 14 Receiver Index*/
 
 };
 
-//  Version 2.1 record description:
+/*  Version 2.1 record description:*/
 struct linepars pparse2[] = {
-      {  0, 1, "%c","RecTyp"   },  //  0 Label (R or S)
-      {  1,10, "%f","LName"    },  //  1 Line name       (float in std)
-      { 11,10, "%f","PNum"     },  //  2 Point number    (float in std)
-      { 23, 1, "%d","PInd"     },  //  3 Point index
-      { 24, 2, "%d","PCode"    },  //  4 Point code      (string in std)
-      { 26, 4, "%d","StaCor"   },  //  5 Static Correction
-      { 30, 4, "%f","PDepth"   },  //  6 Point Depth
-      { 34, 4, "%d","Datum"    },  //  7 Seismic datum
-      { 38, 2, "%d","UpHole"   },  //  8 Uphole Time
-      { 40, 6, "%f","WDept"    },  //  9 Water Depth
-      { 46, 9, "%lf","MapX"    },  // 10 Map Grid Easting
-      { 55,10, "%lf","MapY"    },  // 11 Map Grid Northing
-      { 65, 6, "%f","Elev"     },  // 12 Surface Elevation
-      { 71, 3, "%d","YDay"     },  // 13 Day of Year
-      { 74, 6, "%d","Time"     }   // 14 Time of day     (3I2 in std)
+      {  0, 1, "%c","RecTyp"   },  /*  0 Label (R or S)*/
+      {  1,10, "%f","LName"    },  /*  1 Line name       (float in std)*/
+      { 11,10, "%f","PNum"     },  /*  2 Point number    (float in std)*/
+      { 23, 1, "%d","PInd"     },  /*  3 Point index*/
+      { 24, 2, "%d","PCode"    },  /*  4 Point code      (string in std)*/
+      { 26, 4, "%d","StaCor"   },  /*  5 Static Correction*/
+      { 30, 4, "%f","PDepth"   },  /*  6 Point Depth*/
+      { 34, 4, "%d","Datum"    },  /*  7 Seismic datum*/
+      { 38, 2, "%d","UpHole"   },  /*  8 Uphole Time*/
+      { 40, 6, "%f","WDept"    },  /*  9 Water Depth*/
+      { 46, 9, "%lf","MapX"    },  /* 10 Map Grid Easting*/
+      { 55,10, "%lf","MapY"    },  /* 11 Map Grid Northing*/
+      { 65, 6, "%f","Elev"     },  /* 12 Surface Elevation*/
+      { 71, 3, "%d","YDay"     },  /* 13 Day of Year*/
+      { 74, 6, "%d","Time"     }   /* 14 Time of day     (3I2 in std)*/
 
 };
 
 struct linepars xparse2[] = {
-      {  0, 1, "%c","RecTyp"   }, //  0 Label X
-      {  1, 6, "%s","TapeNo"   }, //  1 Tape Number     (Sting in std)
-      {  7, 8, "%d","RecNum"   }, //  2 Field Record Number
-      { 15, 1, "%d","RecInc"   }, //  3 Field Record Increment
-      { 16, 1, "%c","InstCode" }, //  4 Instrument Code (string in std)
-      { 17,10, "%f","SLName"   }, //  5 Source Line Name(float in std)
-      { 27,10, "%f","PNum"     }, //  6 Point number    (float in std)
-      { 37, 1, "%d","PIndex"   }, //  7 Point Index
-      { 38, 5, "%d","FromChan" }, //  8 From Channel
-      { 43, 5, "%d","ToChan"   }, //  9 To Channel
-      { 48, 1, "%d","ChanInc"  }, // 10 Channel Increment
-      { 49,10, "%f","RLName"   }, // 11 Receiver Line name(float in std)
-      { 59,10, "%f","FromRecv" }, // 12 From Receiver   (float in std)
-      { 69,10, "%f","ToRecv"   }, // 13 To Receiver     (float in std)
-      { 79, 1, "%d","RenIx"    }  // 14 Receiver Index
+      {  0, 1, "%c","RecTyp"   }, /*  0 Label X*/
+      {  1, 6, "%s","TapeNo"   }, /*  1 Tape Number     (Sting in std)*/
+      {  7, 8, "%d","RecNum"   }, /*  2 Field Record Number*/
+      { 15, 1, "%d","RecInc"   }, /*  3 Field Record Increment*/
+      { 16, 1, "%c","InstCode" }, /*  4 Instrument Code (string in std)*/
+      { 17,10, "%f","SLName"   }, /*  5 Source Line Name(float in std)*/
+      { 27,10, "%f","PNum"     }, /*  6 Point number    (float in std)*/
+      { 37, 1, "%d","PIndex"   }, /*  7 Point Index*/
+      { 38, 5, "%d","FromChan" }, /*  8 From Channel*/
+      { 43, 5, "%d","ToChan"   }, /*  9 To Channel*/
+      { 48, 1, "%d","ChanInc"  }, /* 10 Channel Increment*/
+      { 49,10, "%f","RLName"   }, /* 11 Receiver Line name(float in std)*/
+      { 59,10, "%f","FromRecv" }, /* 12 From Receiver   (float in std)*/
+      { 69,10, "%f","ToRecv"   }, /* 13 To Receiver     (float in std)*/
+      { 79, 1, "%d","RenIx"    }  /* 14 Receiver Index*/
 
 };
 
@@ -127,7 +127,7 @@ int getSPSfield(char *Line, int rev, int idx, void *addr)
    char tmp;
 
    if (rev != 0 && rev != 2) {
-         // throw a tantrum
+         /* throw a tantrum*/
          fprintf(stderr,"****  Unsupported SPS format %d!!\n\n",rev);
          return 1;
    }
@@ -163,7 +163,7 @@ int getSPSfield(char *Line, int rev, int idx, void *addr)
       ilast = strlen(Line);  /*  Just reduce the parse length */
    }
 
-   // Save the starting character of next field
+   /* Save the starting character of next field*/
    tmp = Line[ilast];
 
    Line[ilast] = '\0';
