f0484d10b8c9689dd750621a2e2fc594a4775615
Yangkang Chen <chenyk1990@gmail.com>
2020-09-16 16:17:55 -0500

    2020年 9月16日 星期三 16时17分41秒 CDT

--- reproduce/nonMada/dlmtask/Norm.m
+++
@@ -1,7 +0,0 @@
-function [NDnorm,min_val,max_val] = Norm(ND)
-
-min_val=min(min(min(ND)));
-max_val=max(max(max(ND)));
-NDnorm=(ND-min_val)/(max_val-min_val);
-
-return
--- reproduce/nonMada/dlmtask/iNorm.m
+++
@@ -1,7 +0,0 @@
-function [DCOut] = iNorm(DC,min_val,max_val)
-
-DCOut=max(DC,0); DCOut=min(DC,1);
-DCOut=DCOut*(max_val-min_val)+min_val;
-
-return
-
