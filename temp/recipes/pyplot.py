ba20ffca9862998b0f8989c8f31f847684c72d83
Yangkang Chen <chenyk1990@gmail.com>
2022-09-26 10:57:23 -0500

    Mon Sep 26 10:57:21 CDT 2022

---
+++ temp/recipes/pyplot.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure(figsize=(16, 8))
+ax=plt.subplot(5,2,1)
+plt.imshow(cmpn.reshape(100,500,order='F'),cmap='jet',clim=(-0.2, 0.2),aspect=0.8)
+plt.title('Raw data',color='k');ax.set_xticks([]);ax.set_yticks([]);
+ax=plt.subplot(5,2,3)
+plt.imshow(dipi.reshape(100,500,order='F'),cmap='jet',clim=(-1,1),aspect=0.8)
+plt.title('Iline slope',color='k');ax.set_xticks([]);ax.set_yticks([]);
+ax=plt.subplot(5,2,5)
+plt.imshow(dipx.reshape(100,500,order='F'),cmap='jet',clim=(-1,1),aspect=0.8)
+plt.title('Xline slope',color='k');ax.set_xticks([]);ax.set_yticks([]);
+ax=plt.subplot(5,2,7)
+plt.imshow(cmpn_d1.reshape(100,500,order='F'),cmap='jet',clim=(-0.2, 0.2),aspect=0.8)
+plt.title('Filtered (SOMEAN)',color='k');ax.set_xticks([]);ax.set_yticks([]);
+ax=plt.subplot(5,2,9)
+plt.imshow((cmpn-cmpn_d1).reshape(100,500,order='F'),cmap='jet',clim=(-0.2, 0.2),aspect=0.8)
+plt.title('Noise (SOMEAN)',color='k');ax.set_xticks([]);ax.set_yticks([]);
+
+
+# ax=plt.subplot(5,2,2)
+# plt.imshow(cmpn.reshape(100,500,order='F'),cmap='jet',clim=(-0.2, 0.2),aspect=0.8)
+# plt.title('Raw data',color='k');ax.set_xticks([]);ax.set_yticks([]);
+# ax=plt.subplot(5,2,4)
+# plt.imshow(dipi.reshape(100,500,order='F'),cmap='jet',clim=(-1,1),aspect=0.8)
+# plt.title('Iline slope',color='k');ax.set_xticks([]);ax.set_yticks([]);
+# ax=plt.subplot(5,2,6)
+# plt.imshow(dipx.reshape(100,500,order='F'),cmap='jet',clim=(-1,1),aspect=0.8)
+# plt.title('Xline slope',color='k');ax.set_xticks([]);ax.set_yticks([]);
+ax=plt.subplot(5,2,8)
+plt.imshow(cmpn_d2.reshape(100,500,order='F'),cmap='jet',clim=(-0.2, 0.2),aspect=0.8)
+plt.title('Filtered (SOMF)',color='k');ax.set_xticks([]);ax.set_yticks([]);
+ax=plt.subplot(5,2,10)
+plt.imshow((cmpn-cmpn_d2).reshape(100,500,order='F'),cmap='jet',clim=(-0.2, 0.2),aspect=0.8)
+plt.title('Noise (SOMF)',color='k');ax.set_xticks([]);ax.set_yticks([]);
+plt.savefig('test_pyseistr_somf3d.png',format='png',dpi=300)
+
+plt.show()
+
