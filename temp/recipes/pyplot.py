8f24d65cb4d7e2abefa7ee02b945f0899dd686fa
Yangkang Chen <chenyk1990@gmail.com>
2022-10-25 10:20:05 -0500

    Tue Oct 25 10:20:01 CDT 2022

--- temp/recipes/pyplot.py
+++ temp/recipes/pyplot.py
@@ -46,11 +46,73 @@ plt.show()
 ## Important one
 plt.gca().set_yticks([]);
 
+plt.figure;
+plt.plot(lons,lats,'sr');
+plt.plot(lons0,lats0,'pk');
+plt.show()
 
+plot(x, y, 'go--', linewidth=2, markersize=12)
+plot(x, y, color='green', marker='o', linestyle='dashed',
+     linewidth=2, markersize=12)
+     
+     
 
 e.write("test.qml",format="QUAKEML")
 
 
+## in plot.waveform
 
+	ntr=len(st)
+	
+	fig = plt.figure(figsize=(6, 8))
+	
+	for ii in range(ntr):
+		ax = plt.subplot(ntr,1,ii+1)
+		nt=len(st[ii].data);
+		twin=(nt-1)*1.0/st[ii].stats.sampling_rate;
+		staname=st[ii].stats.network+'.'+st[ii].stats.station
+		t=np.linspace(0,twin,nt)
+		plt.plot(t,st[ii].data,color='k',label = st[ii].id, linewidth = 1, markersize=1)
+		
+		if ii==0 and titleoff != 1:
+			plt.title(st[ii].stats.starttime,fontsize='large', fontweight='normal')
+		ax.legend(loc='lower right', fontsize = 10/(ntr/4))
+		if ii==ntr-1:
+			if axoff == 1:
+				plt.setp(ax.get_xticklabels(), visible=False)
+			else:
+				plt.setp(ax.get_xticklabels(), visible=True)
+				ax.set_xlabel("Time (s)",fontsize='large', fontweight='normal')
+		else:
+			plt.setp(ax.get_xticklabels(), visible=False)
+		ax.set_xlim(xmin=0)
+		ax.set_xlim(xmax=t[-1])
+		ymin, ymax = ax.get_ylim()
+		
+		if ayoff:
+			plt.setp(ax.get_yticklabels(), visible=False)
+			
+		if picks is not None:
+			if picks[ii]['P'] is not None:
+				tp=picks[ii]['P']-st[ii].stats.starttime
+				plt.vlines(tp, ymin, ymax, color = 'r', linewidth = 1) #for P
+			
+			if picks[ii]['S'] is not None:
+				ts=picks[ii]['S']-st[ii].stats.starttime
+				plt.vlines(ts, ymin, ymax, color = 'g', linewidth = 1) #for S
+# 		plt.text(2.5,(ymin+ymax)/2,staname)
+
+		if ptime is not None:
+			tp=ptime[ii]-st[ii].stats.starttime
+			plt.vlines(tp, ymin, ymax, color = 'r', linewidth = 1) #for P
+			
+		if stime is not None:
+			ts=stime[ii]-st[ii].stats.starttime
+			plt.vlines(ts, ymin, ymax, color = 'g', linewidth = 1) #for P
+			
+	if figname is not None:
+		plt.savefig(figname,**kwargs)
+		
+		
 
 
