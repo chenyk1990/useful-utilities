570830f7e5012cb283c464a7f8d778ab1efffd5c
Yangkang Chen <chenyk1990@gmail.com>
2023-02-14 11:01:58 -0600

    Tue Feb 14 11:01:55 CST 2023

--- temp/recipes/pyplot.py
+++ temp/recipes/pyplot.py
@@ -131,6 +131,29 @@ fid = open ("syn3d_dc.bin", "wb") #binary file format, int
 fid.write(d0.flatten(order='F'))
 
 
+## text box
+def addtext(ax, props):
+    ax.text(0.5, 0.5, 'text 0', props, rotation=0)
+    ax.text(1.5, 0.5, 'text 45', props, rotation=45)
+    ax.text(2.5, 0.5, 'text 135', props, rotation=135)
+    ax.text(3.5, 0.5, 'text 225', props, rotation=225)
+    ax.text(4.5, 0.5, 'text -45', props, rotation=-45)
+    for x in range(0, 5):
+        ax.scatter(x + 0.5, 0.5, color='r', alpha=0.5)
+    ax.set_yticks([0, .5, 1])
+    ax.set_xticks(np.arange(0, 5.1, 0.5))
+    ax.set_xlim(0, 5)
+    ax.grid(True)
+
+
+# the text bounding box
+bbox = {'fc': '0.8', 'pad': 0}
+
+fig, axs = plt.subplots(2, 1, sharex=True)
+
+addtext(axs[0], {'ha': 'center', 'va': 'center', 'bbox': bbox})
+
+
 ## compare with matlab
 import scipy
 from scipy import io
