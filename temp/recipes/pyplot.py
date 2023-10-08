d79e886c0083486526bcd764e6ef954ab5fd86e5
Yangkang Chen <chenyk1990@gmail.com>
2023-10-07 19:11:44 -0500

    Sat Oct  7 19:11:42 CDT 2023

--- temp/recipes/pyplot.py
+++ temp/recipes/pyplot.py
@@ -301,4 +301,16 @@ ax = fig.add_subplot(projection='3d')
 ax.scatter(xs, ys, zs, c=amps)
 plt.show()
 
+%% add patch
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+#define Matplotlib figure and axis
+fig, ax = plt.subplots()
+#create simple line plot
+ax.plot([0, 10],[0, 10])
+#add rectangle to plot
+ax.add_patch(Rectangle((1, 1), 2, 6, alpha=0.5)) #xy,width,height
+#display plot
+plt.show()
+
 
